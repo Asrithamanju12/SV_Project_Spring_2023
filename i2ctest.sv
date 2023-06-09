aad67429fb7239e6149aa8ccd37201351ec8b043
Asrithamanju12 <127574592+Asrithamanju12@users.noreply.github.com>
2023-06-08 17:01:19 -0700

    Update i2ctest.sv

--- i2ctest.sv
+++ i2ctest.sv
@@ -21,25 +21,35 @@ module i2ctest();
 	end
 
 	initial begin
+	//RESET
 		repeat(5) @(negedge clk);
 		reset = '1;
 		repeat(5) @(negedge clk);
 		reset = '0;
 		repeat(5) @(negedge clk);
-
+	//Asserign wr_en
 		wr_en = '0;
 		repeat(5) @(negedge clk);
-		wr_en = '1;
-		addr = 6'b00_1101;
-		data = 8'b1001_1011;
+		wr_en = '1
+	//Stimulus for write operation
+		repeat(10)
+		begin
+		addr = generateAddress();
+		data = generateData();
 		repeat(1) @(negedge clk);
+		end
+		
 		wr_en = '0;
 		repeat(35) @(negedge clk);
-
+	
+	//Stimulus for read operation
 		rd_en = '1;
-		addr = 6'b01_0011;
-
+		repeat(10)
+		begin
+		addr = generateAddress();
 		repeat(1) @(negedge clk);
+		end
+		
 		rd_en = '0;
 
 		repeat(25) @(negedge clk);
@@ -50,6 +60,8 @@ module i2ctest();
 
 	assign sda = sda_en ? '0 : 'z;
 
+	//Acknowledge Signal Generation:
+	//While Individually testing the I2C interface, the ack signal which should be received from the memory controller is generated in the test bench
 	always @(posedge clk) begin
 		if((i2c.state == 'd5) && (i2c.next_state == 'd5))
 			sda_en = 1'b1;
@@ -57,5 +69,13 @@ module i2ctest();
 			sda_en = 1'b0;
 	end
 
+
+	function logic [ADDRWIDTH-1:0] generateAddress;
+		generateAddress = $urandom_range(0, (1 << ADDRWIDTH));
+	endfunction
+
+	function logic [DATAWIDTH-1:0] generateData;
+		generateData = $urandom_range(0,(1<<DATAWIDTH)));
+	endfunction
 
 endmodule : i2ctest
